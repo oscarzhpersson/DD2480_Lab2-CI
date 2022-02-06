a93a952b708c6b802f06026455cf7da40cabf9ab
Oscar Persson <opers@kth.se>
2022-02-06 22:12:14 +0100

    FEAT - Issue#7: Adds functionality to clone git repository in CWD or else specified

--- continousIntegration/main.py
+++ continousIntegration/main.py
@@ -10,6 +10,8 @@ STATUS_DEBUG = True
 HOST = "127.0.0.1"
 PORT = 8080
 
+PATH_REPO = '' # Folder within CWD to run the code.
+
 ##### PROGRAM #####
 
 app = Flask(__name__) # Variable for flask server application, to be called upon.
@@ -25,15 +27,19 @@ def handler_Push():
     # TODO: Extract Repository.
     # TODO: Save relevant parts of repo into variables to send to the other scripts.
 
-    branch = data["repository"]["clone_url"] # Fetches the clone URL from the payload.
-
-    print(branch)
-    return branch
-
     # Here you do all the continuous integration tasks
     # For example:
     # 1st clone your repository
     # 2nd compile the code
+
+    # Step 1: Clone the repository.
+    branch = data["repository"]["clone_url"] # Fetches the clone URL from the payload.
+
+    os.chdir(os.getcwd + PATH_REPO) # Changes current directory to where the cloned repository is to be located.
+    os.system("git clone " + branch) # Runs command to clone the repository.
+
+    # TODO: Run module that compiles.
+    # TODO: Run module that tests.
 
     #Flask.Response(status=200)
     return "OK" # Defaults to 200 response code.

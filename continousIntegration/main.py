0c14cb4af1837a1203d43aea773fb1d802acc4f9
Oscar Persson <opers@kth.se>
2022-02-06 18:48:35 +0100

    FIX - Issue#7: Adds a working basic webserver implementation
    The previous implementation had some errors which this fixes

--- continousIntegration/main.py
+++ continousIntegration/main.py
@@ -7,21 +7,28 @@ import logging
 
 app = Flask(__name__)
 
-@app.route('/') # Triggered by URL localhost:5000/
-def default():
-    return "Server up and running"
-
-
 # Create an endpoint which receives requests from the GitHub API.
-
-@app.route('/githubPush', methods=['POST']) # Triggered by URL localhost:5000/githubPush.
+@app.route('/', methods=['POST']) # Triggered by URL localhost:5000/
 def handler_Push():
 
     data = request.json # Request the data from the event.
 
     app.logger.info("Received PUSH event from webhook!") # Debug print.
 
-    return data
+    return "OK" #Flask.Response(status=200)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+##### Things that DID NOT work #####
+
+#@app.route('/githubPush', methods=['POST']) # Triggered by URL localhost:5000/githubPush.
+#def default():
+#    return "Server up and running"

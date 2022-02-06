fd283599fb388ff3bd6802dcd0dad9248f0c44e7
Oscar Persson <opers@kth.se>
2022-02-06 23:37:42 +0100

    FEAT - Issue#4: Adds functionality to fetch all .py files from a given directory

---
+++ continousIntegration/modules/compilation.py
@@ -0,0 +1,16 @@
+# To be called from main.py.
+# Compiles files within the cloned repository which is specified using the path in the PATH argument.
+
+def compile(PATH):
+
+    # Create a list to store all file paths.
+    pythonFiles = []
+
+    # Fetch all python files.
+    for root, dirs, files in os.walk(PATH): # Traverse directory storing relevant information, from PATH path.
+        for file in files: # Go through all files in current directory.
+
+            if file.endswith('.py'): # If the file is of type .py.
+                pythonFiles.append(os.path.join(root, file)) # Add it to the list as a complete file path.
+
+    print("")

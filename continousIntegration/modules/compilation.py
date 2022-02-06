de81d9f97843432b8d2728e710c063e07964fc62
Oscar Persson <opers@kth.se>
2022-02-06 23:40:14 +0100

    FEAT - Issue#4: Adds imports for compilation unit

--- continousIntegration/modules/compilation.py
+++ continousIntegration/modules/compilation.py
@@ -1,3 +1,10 @@
+##### IMPORTS #####
+
+import os
+import sys
+
+##### PROGRAM #####
+
 # To be called from main.py.
 # Compiles files within the cloned repository which is specified using the path in the PATH argument.
 
@@ -13,4 +20,4 @@ def compile(PATH):
             if file.endswith('.py'): # If the file is of type .py.
                 pythonFiles.append(os.path.join(root, file)) # Add it to the list as a complete file path.
 
-    print("")
+    print('')

59f0bc40b519cc4f480bbf738acbd1423259c8e9
soderqw <soderqvistw@gmail.com>
2022-02-07 15:51:28 +0100

    TEST - Issue #6: fixes typo

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -4,13 +4,14 @@ import requests
 from flask import Flask, request, json
 
 def notify(REPO, NAME, data):
-    SHA = data["after"]
+    SHA = str(data["after"])
     #REPO = data["repository"]["full_name"]
     GIT_API = "https://api.github.com"
     
     # os.system("curl " + "-X POST" + "-H Accept: application/vnd.github.v3+json" + "https://api.github.com/repos/{REPO}/{SHA}" + "-d {'state':'success'}")
     
-    r = requests.post(GIT_API + "/repos/" + NAME + "/" + REPO + "/statuses/" + SHA)
+    r = requests.post(GIT_API + "/repos/" + str(NAME) + "/" + str(REPO) + "/statuses/" + SHA)
+    print(GIT_API + "/repos/" + str(NAME) + "/" + str(REPO) + "/statuses/" + SHA)
     print(r.text)
 
     #/repos/{owner}/{repo}/statuses/{sha}

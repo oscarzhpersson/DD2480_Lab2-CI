e4a146ef7eee0f849cd72dbd1c2362ca53372805
soderqw <soderqvistw@gmail.com>
2022-02-07 15:38:25 +0100

    TEST - Issue #6: tests and checks if sha from payload is correct

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -1,11 +1,17 @@
 from itsdangerous import json
 import os
-
+import requests
+from flask import Flask, request, json
 
 def notify(PATH, data):
     SHA = data["after"]
+    print(SHA)
     REPO = data["repository"]["full_name"]
 
-    os.system("curl " +  "-X POST" + "-H Accept: application/vnd.github.v3+json" + "https://api.github.com/repos/{REPO}/{SHA}" + "-d {'state':'success'}")
+    # os.system("curl " + "-X POST" + "-H Accept: application/vnd.github.v3+json" + "https://api.github.com/repos/{REPO}/{SHA}" + "-d {'state':'success'}")
+    
+    r = requests.get()
+
+    #/repos/{owner}/{repo}/statuses/{sha}
 
     return "OK"

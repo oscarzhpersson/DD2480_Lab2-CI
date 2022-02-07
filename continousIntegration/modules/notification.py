adf07845d28a9b5484da1d03f02b7cff7ebb6485
soderqw <soderqvistw@gmail.com>
2022-02-07 14:45:34 +0100

    FIX - Issue #6: fixes fstring formatting

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -5,7 +5,6 @@ import os
 def notify(PATH, data):
     SHA = data["after"]
     REPO = data["repository"]["full_name"]
-    ACCEPT = f"curl -X POST -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/{REPO}/{SHA} -d '{'state':'success'}'"
-    os.system(ACCEPT)
+    os.system(f'curl -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/{REPO}/{SHA} -d "{"state":"success"}" ')
 
     return "OK"

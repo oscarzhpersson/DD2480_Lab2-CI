3422af623b4ce35927503d7eb7e0db98ee8f3778
soderqw <soderqvistw@gmail.com>
2022-02-07 18:28:52 +0100

    TEST - Issue #6: fixes typo

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -5,21 +5,22 @@ from flask import Flask, request, json
 
 def notify(data):
     SHA = str(data["after"])
-    REPO = data["repository"]["full_name"]
+    REPO = str(data["repository"]["full_name"])
     GIT_API = "https://api.github.com"
-    query = GIT_API + "/repos/" + REPO + "/statuses/" + SHA
+    REF = str(data["ref"])
+    query_post = GIT_API + "/repos/" + REPO + "/statuses/" + SHA
+    query_get = GIT_API + "/repos/" + REPO + "/commits/" + REF + "/statuses"
     
     # os.system("curl " + "-X POST" + "-H Accept: application/vnd.github.v3+json" + "https://api.github.com/repos/{REPO}/{SHA}" + "-d {'state':'success'}")
     
-    response = requests.post(query,
+    response = requests.post(query_post,
             data = {'state':'pending'})
     print(response.json())
     print(response.headers)
-
     print("---")
 
-    r = requests.get(GIT_API + "/repos/" + REPO + "/statuses/" + SHA)
-    print(GIT_API + "/repos/" + REPO + "/statuses/" + SHA)
+    #/repos/{owner}/{repo}/commits/{ref}/statuses
+    r = requests.get(query_get)
     print(r.status_code)
     print(r.json())
 

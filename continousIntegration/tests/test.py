a1bc65ab504f9242ed21056c9f9c3c7913c51b22
Tim Jonsson <timjonss@kth.se>
2022-02-07 15:01:23 +0100

    ADD - Issue #5: Adds test cases for successful tests and failed tests

--- continousIntegration/tests/test.py
+++ continousIntegration/tests/test.py
@@ -11,7 +11,15 @@ class Tests(unittest.TestCase):
     def test1(self):
         status , _ = test('./test1')
         self.assertTrue(status == 'SUCCESS')
-        
+
+    def test2(self):
+        status , _ = test('./test2')
+        self.assertTrue(status == 'SUCCESS')
+
+    def test3(self):
+        status , _ = test('./test3')
+        self.assertTrue(status == 'ERROR')
+    
 
 if __name__ == '__main__':
     unittest.main()

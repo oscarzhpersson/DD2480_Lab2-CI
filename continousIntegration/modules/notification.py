8399a87b8eed5512eff50456842f2c67a181d971
soderqw <soderqvistw@gmail.com>
2022-02-07 15:49:33 +0100

    TEST - Issue #6: fixes typo

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -8,12 +8,10 @@ def notify(REPO, NAME, data):
     #REPO = data["repository"]["full_name"]
     GIT_API = "https://api.github.com"
     
-
     # os.system("curl " + "-X POST" + "-H Accept: application/vnd.github.v3+json" + "https://api.github.com/repos/{REPO}/{SHA}" + "-d {'state':'success'}")
     
     r = requests.post(GIT_API + "/repos/" + NAME + "/" + REPO + "/statuses/" + SHA)
     print(r.text)
-
 
     #/repos/{owner}/{repo}/statuses/{sha}
 

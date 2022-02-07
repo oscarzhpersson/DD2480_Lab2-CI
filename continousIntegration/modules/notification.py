e4f9cb5882e38cc255525fe37af17e3d42d857db
soderqw <soderqvistw@gmail.com>
2022-02-07 18:11:18 +0100

    TEST - Issue #6: fixes typo

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -7,9 +7,16 @@ def notify(data):
     SHA = str(data["after"])
     REPO = data["repository"]["full_name"]
     GIT_API = "https://api.github.com"
+    query = GIT_API + "/repos/" + REPO + "/statuses/" + SHA
     
     # os.system("curl " + "-X POST" + "-H Accept: application/vnd.github.v3+json" + "https://api.github.com/repos/{REPO}/{SHA}" + "-d {'state':'success'}")
     
+    response = requests.post(query,
+            data = {'key1':'value1', 'key2':'value2'},
+            state = "pending")
+    print(response.headers)
+
+    requests.post()
     r = requests.get(GIT_API + "/repos/" + REPO + "/statuses/" + SHA)
     print(GIT_API + "/repos/" + REPO + "/statuses/" + SHA)
     print(r.status_code)

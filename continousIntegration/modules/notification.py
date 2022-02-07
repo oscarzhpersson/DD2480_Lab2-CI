d7bd82c337a6e2f1196b07e793ad96758f4c221b
soderqw <soderqvistw@gmail.com>
2022-02-07 14:59:03 +0100

    FIX - Issue #6: fixes string formatting

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -6,6 +6,6 @@ def notify(PATH, data):
     SHA = data["after"]
     REPO = data["repository"]["full_name"]
 
-    os.system("curl" +  "-X POST" + "-H Accept: application/vnd.github.v3+json" + "https://api.github.com/repos/{REPO}/{SHA}" + "-d {'state':'success'}")
+    os.system("curl " +  "-X POST" + "-H Accept: application/vnd.github.v3+json" + "https://api.github.com/repos/{REPO}/{SHA}" + "-d {'state':'success'}")
 
     return "OK"

10074ee59e8034b76f72dd109bacd6f212388bc8
soderqw <soderqvistw@gmail.com>
2022-02-08 15:03:27 +0100

    FIX - Issue #6: fixes curl command in notifications.py

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -7,16 +7,20 @@ from flask import Flask, request, json
 def notify(data, branch, TOKEN):
     SHA = str(data["after"])
     REPO = str(data["repository"]["full_name"])
+    CLIENT_ID = str(data["repository"]["id"])
     GIT_API = "https://api.github.com"
-
+    
     query_post = GIT_API + "/repos/" + REPO + "/statuses/" + SHA
     query_get = GIT_API + "/repos/" + REPO + "/commits/" + SHA + "/statuses"
 
     # Oath token generation
-    r = requests.post("https://github.com/login/device/code")
+    r = requests.post("https://github.com/login/device/code", data={'client_id':CLIENT_ID, 'scope':'repo'})
+    print("---")
+    print(r.status_code)
+    print("---")
 
     # Create a commit status using curl
-    state = r"\"state\":\"success\",\"target_url\":\"https://bc51-78-69-113-83.ngrok.io\"" #"{\"state\": \"failure\" }" #"'state':'failure'"
+    state = r"\"state\":\"success\"" 
     command = 'curl -H "Authorization: token ' + TOKEN + '" "Content-Type: application/json"   -X POST -d "{'+ state +'}" https://api.github.com/repos/' + REPO +'/statuses/' + SHA
     print("---")
     print(command)

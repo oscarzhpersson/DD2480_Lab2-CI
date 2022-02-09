abb80ef00ca22749411121ac6d915825bea06e45
Mustafa Ali <62649206+mustafamusse@users.noreply.github.com>
2022-02-09 15:18:59 +0100

    FIX - Issue #28: Fixed the file path for when checking where the logger file is

--- continousIntegration/modules/compilation.py
+++ continousIntegration/modules/compilation.py
@@ -73,11 +73,11 @@ def logger(PATH_REPO, name, message, sender, sha):
     '''
 
     #Checks if the file exist
-    file_exists = os.path.exists('../resources/logging.txt') #will hold a True or False value
+    file_exists = os.path.exists('../../../logging.txt') #will hold a True or False value
 
     #Creates the file if it does not exist
     if(file_exists == False):
-        f = open("../resources/logging.txt", "x")
+        f = open("../../../logging.txt", "x")
 
     # Append-adds at last of the file
     time = datetime.now()

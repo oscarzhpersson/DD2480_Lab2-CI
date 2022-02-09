a43d845b3da204d0d58f31f28b0fdee02e16152c
soderqw <soderqvistw@gmail.com>
2022-02-09 12:10:40 +0100

    FEAT - Issue #6: adds conditional checking for authentication token in notification.py

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -5,9 +5,13 @@ def notify(data, STATUS, TOKEN):
     SHA = str(data["after"])
     REPO = str(data["repository"]["full_name"])
 
+    if not TOKEN: 
+        raise ValueError('No authorization token is provided')
+
     # Create a commit status using curl
     state = fr"\"state\":\"{STATUS}\"" 
-    command = 'curl -H "Authorization: token ' + TOKEN + '" "Content-Type: application/json"   -X POST -d "{'+ state +'}" https://api.github.com/repos/' + REPO +'/statuses/' + SHA
+    command = 'curl -H "Authorization: token ' + TOKEN + '" "Accept: application/vnd.github.v3+json" -X POST -d "{'+ state +'}" https://api.github.com/repos/' + REPO + '/statuses/' + SHA
+    print(command)
     os.system(command)
 
     return "OK"

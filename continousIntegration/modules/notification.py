4760b0af850b506b7f9b0a588b977e39364094fc
soderqw <soderqvistw@gmail.com>
2022-02-09 14:01:29 +0100

    DOCS - Issue #6: adds DocString documentation to the notification function

--- continousIntegration/modules/notification.py
+++ continousIntegration/modules/notification.py
@@ -2,6 +2,15 @@ import os
 from flask import Flask, json
 
 def notify(data, STATUS, TOKEN):
+    ''' Will set commit status to 'pending', 'success' or 'failure' based on STATUS input. The status set will be visible on GitHub in the commit history after a push.
+
+        Parameters
+        ----------
+        data: JSON file containing webhook payload.
+        STATUS: Set by caller to 'pending', 'success', 'failure' or other valid statuses.
+        TOKEN: Set by caller to authenticate in order to make GitHub API calls.
+
+    '''
     SHA = str(data["after"])
     REPO = str(data["repository"]["full_name"])
 
@@ -12,5 +21,3 @@ def notify(data, STATUS, TOKEN):
     state = fr"\"state\":\"{STATUS}\"" 
     command = 'curl -H "Authorization: token ' + TOKEN + '" "Accept: application/vnd.github.v3+json" -X POST -d "{'+ state +'}" https://api.github.com/repos/' + REPO + '/statuses/' + SHA
     os.system(command)
-		
-    return "OK"

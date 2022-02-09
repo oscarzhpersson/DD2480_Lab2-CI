af12a51655053ae59277864e01c63c1d87f81da2
Mustafa Ali <62649206+mustafamusse@users.noreply.github.com>
2022-02-09 16:35:12 +0100

    FEAT - Issue#28: Adds the logic for the logger function

--- continousIntegration/modules/compilation.py
+++ continousIntegration/modules/compilation.py
@@ -1,6 +1,7 @@
 ##### IMPORTS #####
 
 import os
+from datetime import datetime
 
 ##### PARAMETERS #####
 
@@ -51,3 +52,20 @@ def compile(PATH):
 
     # Return a message stating success during compilation.
     return ('SUCCESS', 0)
+
+def logger(PATH_REPO, name, message, sender, sha):
+
+    #Checks if the file exist
+    file_exists = os.path.exists('../../../logging.txt') #will hold a True or False value
+
+    #Creates the file if it does not exist
+    if(file_exists == False):
+        f = open("../../../logging.txt", "x")
+
+    # Append-adds at last of the file
+    time = datetime.now()
+    path = PATH_REPO + '/' + name
+
+    file1 = open("../../../logging.txt", "a")  #append mode
+    file1.write("Push event from: " + sender + "\n" + "Path: " + path + "\n" + "Compiled at:" + str(time) + "\n" + "Status: " + message + "\n" + "Sha: " + sha + "\n\n")
+    file1.close()

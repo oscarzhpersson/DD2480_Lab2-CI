45bd7649fa3e2904fc6c77566dd8eefb657e84f1
tim-jonsson <timejonsson@gmail.com>
2022-02-11 11:47:22 +0100

    BUGFIX - Adds test

--- tests/test.py
+++ tests/test.py
@@ -8,9 +8,7 @@ class Test(unittest.TestCase):
         self.assertTrue(os.path.isdir('../DD2480_LAB2-CI'))
 
     def test(self):
-        self.assertFalse(os.path.isdir('../DD2480_LAB2-CI'))
-
-    
+        self.assertTrue(os.path.isdir('../.TOKEN.txt'))
 
 if __name__ == '__main__':
     unittest.main()

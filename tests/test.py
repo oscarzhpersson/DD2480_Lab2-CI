f6e58ef7140cc8d241c31bb2da07e205bbd51c23
Oscar Persson <opers@kth.se>
2022-02-11 13:15:13 +0100

    FEAT - Issue #57: Adds correct import location for test file

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,20 @@
 import unittest
+import os
+import sys
+
+sys.path.append('continousIntegration/modules')
+import compilation
 
 class Test(unittest.TestCase):
     '''
     ''' 
     def test(self):
+        self.assertTrue(os.path.isdir('../DD2480_LAB2-CI'))
 
-        print("prolog"
-        self.assertTrue(False)
+    def test_Compile(self):
+        print("Prolog")
+        compilation.compile(os.getcwd())
+        
 
 if __name__ == '__main__':
     unittest.main()

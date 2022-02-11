f3fa165193d215b4515ec9d4e1eee18178d64797
tim-jonsson <timejonsson@gmail.com>
2022-02-11 11:41:56 +0100

    BUGFIX - Adds test that fails

--- tests/test.py
+++ tests/test.py
@@ -7,5 +7,10 @@ class Test(unittest.TestCase):
     def test(self):
         self.assertTrue(os.path.isdir('../DD2480_LAB2-CI'))
 
+    def test(self):
+        self.assertFalse(os.path.isdir('../DD2480_LAB2-CI'))
+
+    
+
 if __name__ == '__main__':
     unittest.main()

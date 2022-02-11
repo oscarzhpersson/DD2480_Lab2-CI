c4a8d6b8c5658a169b8a9246f9b47dc762e5091c
Tim Jonsson <timjonss@kth.se>
2022-02-11 02:11:32 +0100

    f

--- continousIntegration/tests/test.py
+++ continousIntegration/tests/test.py
@@ -1,1 +1,10 @@
-print()
+import unittest
+
+class Test(unittest.TestCase):
+    '''
+    ''' 
+    def test(self):
+        self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()

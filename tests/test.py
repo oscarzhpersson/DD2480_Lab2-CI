b06ee5fd46e87b9f0f17449b3fb32e8de3bfcc9e
Oscar Persson <opers@kth.se>
2022-02-11 11:08:33 +0100

    Presentation

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class Test(unittest.TestCase):
+    '''
+    ''' 
+    def test(self):
+
+        print("prolog"
+        self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()
